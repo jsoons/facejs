a0f1c4feb986865356106139e3bceca1e5ef90b0
Jan Soons <jsoons@gmail.com>
2020-11-02 13:54:06 +0100

    script loaded

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+const { FaceRecognitionNet, FaceLandmark68Net, ssdMobilenetv1 } = require("face-api.js")
+const { DrawFaceLandmarksOptions } = require("face-api.js/build/commonjs/draw")
+
 const imageUpload = document.getElementById('imageUpload')
+
+Promise.all([
+    faceRecognitionNet.loadFromUri('/models'),
+    faceLandmark68Net.loadFromUri('/models'),
+    ssdMobilenetv1.loadFromUri('/models')
+    
+]).then(start)
+
+function start() {
+    document.body.append('loaded')
+
+}
 
